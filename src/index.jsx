510d37bd09aa999482ac0a03b907c303d0284211
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-19 11:58:27 -0500

    change index.js to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import './index.scss';
 
 // Main component (will eventually use all the others)
-const myFlixApplication = () => {
+const MyFlixApplication = () => {
     return (
         <div className='my-flix'>
             <div>Good morning</div>
@@ -17,4 +17,4 @@ const container = document.querySelector('#root');
 const root = createRoot(container);
 
 // Tells React to render your app in the root DOM element
-root.render(<myFlixApplication />);
+root.render(<MyFlixApplication />);
