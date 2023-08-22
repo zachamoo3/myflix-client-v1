ac7a5af54214d7d0c9ccb6a483fd17f4d49e5927
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-22 11:59:33 -0500

    Update movie-card.jsx

--- components/movie-card/movie-card.jsx
+++ components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div onClick={() => {
@@ -6,4 +8,18 @@ export const MovieCard = ({ movie, onMovieClick }) => {
             {movie.title}
         </div>
     );
+};
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        release_date: PropTypes.string,
+        rating: PropTypes.string,
+        genre: PropTypes.string,
+        director: PropTypes.string,
+        image: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 };
