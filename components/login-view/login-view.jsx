a7f720aad69b817eb7b50c23777edab12697ffc4
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-23 13:09:56 -0500

    Update login-view.jsx

--- components/login-view/login-view.jsx
+++ components/login-view/login-view.jsx
@@ -1,7 +1,13 @@
 import { React, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export const LoginView = ({ onLoggedIn }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleSubmit = (event) => {
+        // this prevents the default behavior of the form which is to reload the page
         event.preventDefault();
 
         const data = {
@@ -32,30 +38,34 @@ export const LoginView = ({ onLoggedIn }) => {
             });
     };
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input
+        <Form onSubmit={handleSubmit}>
+
+            <Form.Group controlId='formUsername'>
+                <Form.Label>Username: </Form.Label>
+                <Form.Control
                     type='text'
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     required
+                    minLength='5'
                 />
-            </label>
-            <label>
-                Password:
-                <input
-                    type='text'
+            </Form.Group>
+
+            <Form.Group controlId='formPassword'>
+                <Form.Label>Password: </Form.Label>
+                <Form.Control
+                    type='password'
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required
                 />
-            </label>
-            <button type='submit'>Submit</button>
-        </form>
+            </Form.Group>
+
+            <Button type='submit'>
+                Submit
+            </Button>
+
+        </Form>
     );
 };
