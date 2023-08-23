e05110e087a30f9c34b888b8754e1f0e2984fe1f
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-22 23:25:04 -0500

    Create login-view.jsx

---
+++ components/login-view/login-view.jsx
@@ -0,0 +1,61 @@
+import { React, useState } from 'react';
+
+export const LoginView = ({ onLoggedIn }) => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            Username: username,
+            Password: password
+        };
+
+        fetch('https://myflix3-8b08c65e975f.herokuapp.com/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Login response: ', data);
+                if (data.user) {
+                    localStorage.setItem('user', JSON.stringify(data.user));
+                    localStorage.setItem('token', data.token);
+                    onLoggedIn(data.user, data.token);
+                } else {
+                    alert('No such user.');
+                }
+            })
+            .catch((e) => {
+                alert('Something went wrong.');
+            });
+    };
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input
+                    type='text'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                    type='text'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </label>
+            <button type='submit'>Submit</button>
+        </form>
+    );
+};
