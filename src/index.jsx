4dd78f1def77da5a6edea91e85f5f0418173875c
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-23 08:48:33 -0500

    Import Bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 // Import components
 import { MainView } from '../components/main-view/main-view.jsx'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 // Import statement to indicate that you need to bundle './index.scss'
 import './index.scss';
 
