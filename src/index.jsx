3c786f0e18c73928e433baca92ef7438621232f4
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-23 13:22:26 -0500

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from '../components/main-view/main-view.jsx'
+import Container from 'react-bootstrap/Container';
 
 // Import statement to indicate that you need to bundle './index.scss'
 import './index.scss';
