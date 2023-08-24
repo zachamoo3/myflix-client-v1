7021613731a0626fd1e5b153cee5a10e1fc338cf
Zachary Moore <zachary.d.moore184@gmail.com>
2023-08-24 15:51:21 -0500

    Update navigation-bar.jsx

--- components/navigation-bar/navigation-bar.jsx
+++ components/navigation-bar/navigation-bar.jsx
@@ -26,6 +26,9 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                                 <Nav.Link as={Link} to='/'>
                                     Home
                                 </Nav.Link>
+                                <Nav.Link as={Link} to='/users'>
+                                    Profile
+                                </Nav.Link>
                                 <Nav.Link onClick={onLoggedOut}>
                                     Logout
                                 </Nav.Link>
