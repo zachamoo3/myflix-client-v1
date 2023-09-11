03f8081bc1228b1048159c0f2fec456619b51892
Zachary Moore <zachary.d.moore184@gmail.com>
2023-09-11 13:21:11 -0500

    Update main-view.jsx

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react';
-import { MovieCard } from '../movie-card/movie-card.jsx';
-import { MovieView } from '../movie-view/movie-view.jsx';
-import { LoginView } from '../login-view/login-view.jsx';
-import { SignupView } from '../signup-view/signup-view.jsx';
-import { NavigationBar } from '../navigation-bar/navigation-bar.jsx';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
+import { NavigationBar } from '../navigation-bar/navigation-bar';
+import { ProfileView } from '../profile-view/profile-view';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
@@ -16,11 +17,36 @@ export const MainView = () => {
     const [token, setToken] = useState(storedToken ? storedToken : null);
 
     const [movies, setMovies] = useState([]);
+    const [favoriteMovies, setFavoriteMovies] = useState([]);
 
     useEffect(() => {
         if (!token) {
             return;
         }
+
+        fetch(`https://myflix3-8b08c65e975f.herokuapp.com/users/${user}`, {
+            method: 'GET',
+            headers: { Authorization: `Bearer ${token}` }
+        })
+            .then((response) => response.json())
+            .then(() => {
+                const userFromApi = ((doc) => {
+                    return {
+                        id: doc._id,
+                        username: doc.Username,
+                        password: doc.Password,
+                        email: doc.Email,
+                        birth_date: doc.Birth_Date,
+                        favorite_movies: doc.Favorite_Movies
+                    };
+                });
+
+                setUser(userFromApi);
+                setFavoriteMovies(user.favorite_movies);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
 
         fetch('https://myflix3-8b08c65e975f.herokuapp.com/movies', {
             headers: { Authorization: `Bearer ${token}` }
@@ -45,6 +71,9 @@ export const MainView = () => {
             .catch((err) => {
                 console.error(err);
             });
+
+        console.log(user);
+        console.log(favoriteMovies);
     }, [token]);
 
 
@@ -61,11 +90,11 @@ export const MainView = () => {
             <Row className='justify-content-md-center'>
                 <Routes>
                     <Route
-                        path="/signup"
+                        path='/signup'
                         element={
                             <>
                                 {user ? (
-                                    <Navigate to="/" />
+                                    <Navigate to='/' />
                                 ) : (
                                     <Col md={5}>
                                         <SignupView />
@@ -75,11 +104,11 @@ export const MainView = () => {
                         }
                     />
                     <Route
-                        path="/login"
+                        path='/login'
                         element={
                             <>
                                 {user ? (
-                                    <Navigate to="/" />
+                                    <Navigate to='/' />
                                 ) : (
                                     <Col md={5}>
                                         <LoginView onLoggedIn={(user) => setUser(user)} />
@@ -89,11 +118,11 @@ export const MainView = () => {
                         }
                     />
                     <Route
-                        path="/movies/:movieId"
+                        path='/movies/:movieId'
                         element={
                             <>
                                 {!user ? (
-                                    <Navigate to="/login" replace />
+                                    <Navigate to='/login' replace />
                                 ) : movies.length === 0 ? (
                                     <Col>The list is empty!</Col>
                                 ) : (
@@ -105,21 +134,53 @@ export const MainView = () => {
                         }
                     />
                     <Route
-                        path="/"
+                        path='/'
                         element={
                             <>
                                 {!user ? (
-                                    <Navigate to="/login" replace />
+                                    <Navigate to='/login' replace />
                                 ) : movies.length === 0 ? (
                                     <Col>The list is empty!</Col>
                                 ) : (
                                     <>
                                         {movies.map((movie) => (
-                                            <Col className="mb-4" key={movie.id} md={3}>
+                                            <Col className='mb-4' key={movie.id} md={3}>
                                                 <MovieCard movie={movie} />
                                             </Col>
                                         ))}
                                     </>
+                                )}
+                            </>
+                        }
+                    />
+                    <Route
+                        path='/users'
+                        element={
+                            <>
+                                {!user ? (
+                                    <Navigate to='/login' replace />
+                                ) : favoriteMovies.length === 0 ? (
+                                    <Col>
+                                        <Row>
+                                            <ProfileView user={user} />
+                                        </Row>
+                                        <Row>
+                                            The list is empty!
+                                        </Row>
+                                    </Col>
+                                ) : (
+                                    <Col>
+                                        <Row>
+                                            <ProfileView user={user} />
+                                        </Row>
+                                        <Row>
+                                            {favoriteMovies.map((movie) => (
+                                                <Col className='mb-4' key={movie.id} md={3}>
+                                                    <MovieCard movie={movie} />
+                                                </Col>
+                                            ))}
+                                        </Row>
+                                    </Col>
                                 )}
                             </>
                         }
