ce6a600161ca4016bc918498d46b16c0beb35669
Zachary Moore <zachary.d.moore184@gmail.com>
2023-09-12 11:19:33 -0500

    Update profile-view.jsx

--- components/profile-view/profile-view.jsx
+++ components/profile-view/profile-view.jsx
@@ -2,26 +2,26 @@ import { React, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
-export const ProfileView = ({ user }) => {
-    var userInfo = user;
+export const ProfileView = ({ user, token, onUserUpdated }) => {
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
-    const [birth_date, setBirth_date] = useState('');
+    const [Username, setUsername] = useState('');
+    const [Password, setPassword] = useState('');
+    const [Email, setEmail] = useState('');
+    const [Birth_Date, setBirth_date] = useState('');
 
-    const handleSubmit = (event) => {
+    const updateUserInfo = (event) => {
         event.preventDefault();
 
         const data = {
-            Username: username,
-            Password: password,
-            Email: email,
-            Birth_Date: birth_date
+            Username: Username,
+            Password: Password,
+            Email: Email,
+            Birth_Date: Birth_Date
         };
 
-        fetch(`https://myflix3-8b08c65e975f.herokuapp.com/users/${userInfo.username}`, {
+        fetch(`https://myflix3-8b08c65e975f.herokuapp.com/users/${user.Username}`, {
             method: 'PUT',
             body: JSON.stringify(data),
             headers: {
@@ -35,40 +35,39 @@ export const ProfileView = ({ user }) => {
             } else {
                 alert('Update failed.');
             }
+        }).then((data) => {
+            console.log('User updated: ', data);
+            localStorage.setItem('user', JSON.stringify(data.user));
+            localStorage.setItem('token', data.token);
+            onUserUpdated(data.user, data.token);
         });
     };
 
     return (
-        <>
-            <Col>
+        <Col>
+            <Row>
+                <h3>User Profile</h3>
                 <div>
                     <span>Username: </span>
-                    <span>{userInfo.username}</span>
-                </div>
-                <div>
-                    <span>Password: </span>
-                    <span>********</span>
+                    <span>{user.Username}</span>
                 </div>
                 <div>
                     <span>Email: </span>
-                    <span>{userInfo.email}</span>
+                    <span>{user.Email}</span>
                 </div>
                 <div>
-                    <span>Birthday: </span>
-                    <span>{userInfo.birth_date}</span>
+                    <span>Date of Birth: </span>
+                    <span>{user.Birth_Date}</span>
                 </div>
-                <div>
-                    <span>Favorite Movies: </span>
-                    <span>{userInfo.favorite_movies}</span>
-                </div>
-            </Col>
-            <Col>
-                <Form onSubmit={handleSubmit}>
+            </Row>
+            <Row>
+                <h3>Update User Information</h3>
+                <Form onSubmit={updateUserInfo}>
                     <Form.Group>
                         <Form.Label>Username: </Form.Label>
                         <Form.Control
                             type='text'
-                            value={username}
+                            value={Username}
                             onChange={(e) => setUsername(e.target.value)}
                             required
                             minLength='5'
@@ -79,7 +78,7 @@ export const ProfileView = ({ user }) => {
                         <Form.Label>Password: </Form.Label>
                         <Form.Control
                             type='password'
-                            value={password}
+                            value={Password}
                             onChange={(e) => setPassword(e.target.value)}
                             required
                         />
@@ -89,7 +88,7 @@ export const ProfileView = ({ user }) => {
                         <Form.Label>Email: </Form.Label>
                         <Form.Control
                             type='email'
-                            value={email}
+                            value={Email}
                             onChange={(e) => setEmail(e.target.value)}
                             required
                         />
@@ -99,7 +98,7 @@ export const ProfileView = ({ user }) => {
                         <Form.Label>Birthday: </Form.Label>
                         <Form.Control
                             type='date'
-                            value={birth_date}
+                            value={Birth_Date}
                             onChange={(e) => setBirth_date(e.target.value)}
                             required
                         />
@@ -109,7 +108,10 @@ export const ProfileView = ({ user }) => {
                         Submit
                     </Button>
                 </Form>
-            </Col>
-        </>
-    )
-}
+            </Row>
+            <Row>
+                <h3>Favorite Movies</h3>
+            </Row>
+        </Col>
+    );
+};
