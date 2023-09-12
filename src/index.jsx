23a790537d6a4d6fe6409b280e10dfcab810b9d5
Zachary Moore <zachary.d.moore184@gmail.com>
2023-09-12 11:20:40 -0500

    moving components folder into src

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { MainView } from '../components/main-view/main-view.jsx'
+import { MainView } from './components/main-view/main-view.jsx'
 import Container from 'react-bootstrap/Container';
 
 // Import statement to indicate that you need to bundle './index.scss'
