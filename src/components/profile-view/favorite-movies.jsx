a3cd612769490687c1b6bee5cc8196a0e4495823
Zachary Moore <zachary.d.moore184@gmail.com>
2023-09-14 11:35:44 -0500

    Favorite Movies display correctly!!!

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -2,75 +2,25 @@ import { React, useState, useEffect } from 'react';
 import Col from 'react-bootstrap/Col';
 import { MovieCard } from '../movie-card/movie-card';
 
-function FavoriteMovies() {
+function FavoriteMovies({ movies }) {
     const storedUser = JSON.parse(localStorage.getItem('user'));
-    const storedToken = localStorage.getItem('token');
 
-    const [filterList, setFilterList] = useState([]); // an array of _id's
-    const [movies, setMovies] = useState([]);
+    let favoriteMovies = storedUser.Favorite_Movies;
 
-
-
-    async function getUserFavorites() {
-        let response = await
-            fetch(`https://myflix3-8b08c65e975f.herokuapp.com/users/${storedUser.Username}`, {
-                method: 'GET',
-                headers: { Authorization: `Bearer ${storedToken}` }
-            });
-        let userData = await response.json();
-        let userFavorites = await userData.Favorite_Movies.map((doc) => {
-            return { title: doc.Title }
-        });
-
-        setFilterList(userFavorites);
-    };
-
-    async function getMovieInfo(doc) {
-        let movieTitleEncoded = encodeURIComponent(doc.title);
-
-        let response = await
-            fetch(`https://myflix3-8b08c65e975f.herokuapp.com/movies/${movieTitleEncoded}`, {
-                method: 'GET',
-                headers: { Authorization: `Bearer ${storedToken}` }
-            });
-        let movieData = await response.json();
-        let movie = {
-            id: movieData._id,
-            title: movieData.Title,
-            release_date: movieData.Release_Date,
-            rating: movieData.Rating,
-            genre: movieData.Genre.Name,
-            director: movieData.Director.Name,
-            image: movieData.Image_Url,
-            description: movieData.Description
-        };
-
-        setMovies(movies => [...movies, movie]);
-    };
-
-    async function makeMovies() {
-        await getUserFavorites();
-
-        filterList.map((doc) => {
-            getMovieInfo(doc);
-        });
-    };
-
-    useEffect(() => {
-        setMovies([]);
-        makeMovies();
-
-        console.log('movies: ', movies); // CONSOLE.LOG !!!
-    }, []);
-
-
+    let filteredMovies = movies.filter(movie => {
+        if (favoriteMovies.includes(movie.id)) {
+            return movie
+        }
+    })
 
     return (
         <>
-            {movies.map((movie) => {
-                <Col className='mb-4' key={movie._id} md={3}>
-                    <MovieCard movie={movie} />
-                </Col>
+            {filteredMovies.map((movie) => {
+                return (
+                    <Col className='mb-4' key={movie.id} md={3}>
+                        <MovieCard movie={movie} />
+                    </Col>
+                )
             })}
         </>
     );
--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -6,7 +6,7 @@ import Row from 'react-bootstrap/Row';
 import DeleteProfile from './delete-profile';
 import FavoriteMovies from './favorite-movies'
 
-export const ProfileView = ({ onDelete }) => {
+export const ProfileView = ({ onDelete, movies }) => {
     const storedUser = JSON.parse(localStorage.getItem('user'));
     const storedToken = localStorage.getItem('token');
 
@@ -70,78 +70,82 @@ export const ProfileView = ({ onDelete }) => {
 
     return (
         <Col>
-            <Row> {/* Display user information */}
-                <h3>User Profile</h3>
-                <div>
-                    <span>Username: </span>
-                    <span>{storedUser.Username}</span>
-                </div>
-                <div>
-                    <span>Email: </span>
-                    <span>{storedUser.Email}</span>
-                </div>
-                <div>
-                    <span>Date of Birth: </span>
-                    <span>{storedUser.Birth_Date.slice(0, 10)}</span>
-                </div>
-            </Row>
-            <Row> {/* Update user info */}
-                <h3>Update User Information</h3>
-                <Form onSubmit={updateUserInfo}>
-                    <Form.Group controlId='formUsername'>
-                        <Form.Label>Username: </Form.Label>
-                        <Form.Control
-                            type='text'
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
-                            minLength='5'
-                        />
-                    </Form.Group>
+            <Row>
+                <Col>
+                    <Row> {/* Display user information */}
+                        <h3>User Profile</h3>
+                        <div>
+                            <span>Username: </span>
+                            <span>{storedUser.Username}</span>
+                        </div>
+                        <div>
+                            <span>Email: </span>
+                            <span>{storedUser.Email}</span>
+                        </div>
+                        <div>
+                            <span>Date of Birth: </span>
+                            <span>{storedUser.Birth_Date.slice(0, 10)}</span>
+                        </div>
+                    </Row>
+                    <br />
+                    <Row> {/* Delete account */}
+                        <h3>Wish to delete your profile?</h3>
+                        <DeleteProfile onDelete={onDelete} />
+                    </Row>
+                </Col>
+                <Col> {/* Update user info */}
+                    <h3>Update User Information</h3>
+                    <Form onSubmit={updateUserInfo}>
+                        <Form.Group controlId='formUsername'>
+                            <Form.Label>Username: </Form.Label>
+                            <Form.Control
+                                type='text'
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                                minLength='5'
+                            />
+                        </Form.Group>
 
-                    <Form.Group controlId='formPassword'>
-                        <Form.Label>Password: </Form.Label>
-                        <Form.Control
-                            type='password'
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </Form.Group>
+                        <Form.Group controlId='formPassword'>
+                            <Form.Label>Password: </Form.Label>
+                            <Form.Control
+                                type='password'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
 
-                    <Form.Group controlId='formEmail'>
-                        <Form.Label>Email: </Form.Label>
-                        <Form.Control
-                            type='email'
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                    </Form.Group>
+                        <Form.Group controlId='formEmail'>
+                            <Form.Label>Email: </Form.Label>
+                            <Form.Control
+                                type='email'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
 
-                    <Form.Group controlId='formBirthDate'>
-                        <Form.Label>Birthday: </Form.Label>
-                        <Form.Control
-                            type='date'
-                            value={birth_date}
-                            onChange={(e) => setBirth_date(e.target.value)}
-                            required
-                        />
-                    </Form.Group>
+                        <Form.Group controlId='formBirthDate'>
+                            <Form.Label>Birthday: </Form.Label>
+                            <Form.Control
+                                type='date'
+                                value={birth_date}
+                                onChange={(e) => setBirth_date(e.target.value)}
+                                required
+                            />
+                        </Form.Group>
 
-                    <Button variant='primary' type='submit'>
-                        Submit
-                    </Button>
-                </Form>
+                        <Button variant='primary' type='submit'>
+                            Update
+                        </Button>
+                    </Form>
+                </Col>
             </Row>
             <Row> {/* Favorite movies */}
                 <h3>Favorite Movies</h3>
-                <FavoriteMovies />
-            </Row>
-            <Row>
-                {/* Delete account */}
-                <h3>Wish to delete your profile?</h3>
-                <DeleteProfile onDelete={onDelete} />
+                <FavoriteMovies movies={movies} />
             </Row>
         </Col>
     );
